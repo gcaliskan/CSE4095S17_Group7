3065eba4f9297aaadec1ffde8fd0dae254794f1c
gcaliskan <caliskansoftware@gmail.com>
2017-04-10 14:12:36 +0300

    Scatter plotlar eklendi

--- Scatter.py
+++ Scatter.py
@@ -7,13 +7,60 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 ## Create data
-df = pd.read_excel("C:/test.xls")
+df = pd.read_excel("C:/orgHR_Data_v1.xls")
 
-# Create a figure instance
-df.plot(kind='scatter', x='number_project', y='time_spend_company');
-fig = plt.figure(1, figsize=(9, 6))
+df1 = pd.read_excel("C:/orgHR_Data_v1.xls")
+df1.plot(kind='scatter', x='satisfaction_level', y='last_evaluation');
+fig1 = plt.figure(1, figsize=(9, 6))
+fig1.savefig('scatterimages/scatter_satisfaction_lastevaluation.png', bbox_inches='tight')
+plt.close(fig1)
 
+df2 = pd.read_excel("C:/orgHR_Data_v1.xls")
+df2.plot(kind='scatter', x='satisfaction_level', y='number_project');
+fig2 = plt.figure(1, figsize=(9, 6))
+fig2.savefig('scatterimages/scatter_satisfaction_numberproject.png', bbox_inches='tight')
+plt.close(fig2)
 
+df2 = pd.read_excel("C:/orgHR_Data_v1.xls")
+df2.plot(kind='scatter', x='satisfaction_level', y='average_montly_hours');
+fig2 = plt.figure(1, figsize=(9, 6))
+fig2.savefig('scatterimages/scatter_satisfaction_averagemontlyhours.png', bbox_inches='tight')
+plt.close(fig2)
+
+df2 = pd.read_excel("C:/orgHR_Data_v1.xls")
+df2.plot(kind='scatter', x='satisfaction_level', y='time_spend_company');
+fig2 = plt.figure(1, figsize=(9, 6))
+fig2.savefig('scatterimages/scatter_satisfaction_timespendcompany.png', bbox_inches='tight')
+plt.close(fig2)
+
+df2 = pd.read_excel("C:/orgHR_Data_v1.xls")
+df2.plot(kind='scatter', x='satisfaction_level', y='Work_accident');
+fig2 = plt.figure(1, figsize=(9, 6))
+fig2.savefig('scatterimages/scatter_satisfaction_workaccident.png', bbox_inches='tight')
+plt.close(fig2)
+
+df2 = pd.read_excel("C:/orgHR_Data_v1.xls")
+df2.plot(kind='scatter', x='satisfaction_level', y='left');
+fig2 = plt.figure(1, figsize=(9, 6))
+fig2.savefig('scatterimages/scatter_satisfaction_left.png', bbox_inches='tight')
+plt.close(fig2)
+
+df2 = pd.read_excel("C:/orgHR_Data_v1.xls")
+df2.plot(kind='scatter', x='satisfaction_level', y='promotion_last_5years');
+fig2 = plt.figure(1, figsize=(9, 6))
+fig2.savefig('scatterimages/scatter_satisfaction_promotionlast5years.png', bbox_inches='tight')
+plt.close(fig2)
+
+df2 = pd.read_excel("C:/orgHR_Data_v1.xls")
+df2.plot(kind='scatter', x='satisfaction_level', y='traffic');
+fig2 = plt.figure(1, figsize=(9, 6))
+fig2.savefig('scatterimages/scatter_satisfaction_traffic.png', bbox_inches='tight')
+plt.close(fig2)
+
+df2 = pd.read_excel("C:/orgHR_Data_v1.xls")
+df2.plot(kind='scatter', x='satisfaction_level', y='remote_work');
+fig2 = plt.figure(1, figsize=(9, 6))
+fig2.savefig('scatterimages/scatter_satisfaction_remotework.png', bbox_inches='tight')
+plt.close(fig2)
 
 # Save the figure
-fig.savefig('scatter_numberproject_timespend.png', bbox_inches='tight')
